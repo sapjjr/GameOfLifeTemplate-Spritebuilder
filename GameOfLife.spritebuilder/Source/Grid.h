a54b1978c58c090993f479c233e6c6ab73d6889e
andrewp <andrew@andrewMac-3.local>
2014-06-26 17:40:02 +0100

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Source/Grid.h
@@ -0,0 +1,15 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by andrew on 26/06/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+
+@end
