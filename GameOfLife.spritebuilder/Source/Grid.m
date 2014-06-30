057c9b5aeba79778ddb708c4444a6111d941bab2
andrewp <andrew@andrewMac-3.local>
2014-06-30 14:34:07 +0100

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -60,12 +60,12 @@ static const int GRID_COLUMNS = 10;
             //creature.isAlive = YES;
             
             x+=_cellWidth;
-            NSLog(@" row  %f ", j);
+            NSLog(@" row  %i ", j);
         }
         
         y += _cellHeight;
         
-        NSLog(@" column  %f " , i);
+        NSLog(@" column  %i " , i);
     }
 }
 
