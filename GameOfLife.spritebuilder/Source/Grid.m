28b868fbc2b75c8c771c4bb83b131807fcc25826
andrewp <andrew@andrewMac-3.local>
2014-06-30 22:02:22 +0100

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -164,7 +164,8 @@ static const int GRID_COLUMNS = 10;
     
     for (int row = 0; row < GRID_ROWS; row++) {
         for (int col = 0; col < GRID_COLUMNS; col++ ) {
-           Creature *creature  ;
+            Creature *creature = _gridArray[row][col];
+
             NSLog(@"row/col (%i, %i)  is alive %hhd and neighbors %i", row,col, creature.isAlive, creature.livingNeighbors);
             //NSLog(@"_creature.livingNeighbors %i ", creature.livingNeighbors );
            
