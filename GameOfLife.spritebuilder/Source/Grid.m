a5b9c85fa836e88a26fb983897cb491457cc4d43
andrewp <andrew@andrewMac-3.local>
2014-06-30 22:24:34 +0100

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -184,7 +184,7 @@ static const int GRID_COLUMNS = 10;
             }
 
 _totalAlive = numAlive;
-NSLog(@"number alive %i", numAlive);
+NSLog(@"Final number alive %i------------", numAlive);
 }
 }
 
