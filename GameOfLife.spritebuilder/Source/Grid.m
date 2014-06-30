eeb914a2953f93d3fa4daa03f43c1e111aa4fd75
andrewp <andrew@andrewMac-3.local>
2014-06-30 20:36:08 +0100

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -168,6 +168,12 @@ static const int GRID_COLUMNS = 10;
             NSLog(@"row %i and Column %i", row,col);
             //Creature *creature = _gridArray[row][col];
             NSLog(@"currentCreature State %ld", (long)_creature.livingNeighbors);
+            
+            if (col == 3) {
+                _creature.isAlive = YES; numAlive ++;
+            } else {
+                if ([_gridArray count] <= 1 || [_gridArray count] >= 4) {
+                    _creature.isAlive = NO;
         }
     
         
