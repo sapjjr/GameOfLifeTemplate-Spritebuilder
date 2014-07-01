31e85d442b8c44179b097a17d43e359d708c05ba
andrewp <andrew@andrewMac-3.local>
2014-07-01 10:58:20 +0100

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -10,8 +10,8 @@
 #import "Creature.h"
 
 // these are variables that cannot be changed
-static const int GRID_ROWS = 12;
-static const int GRID_COLUMNS = 12;
+static const int GRID_ROWS = 8;
+static const int GRID_COLUMNS = 10;
 
 @implementation Grid {
     NSMutableArray *_gridArray;
